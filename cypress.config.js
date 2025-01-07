315906e1a164fda5be5604e511065d7b15fa4f4a
Emerson Carlos Batista <battistta96@gmail.com>
2025-01-06 23:48:54 -0300

    Ajustes na configuração do projeto

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     baseUrl: 'https://www.saucedemo.com',
     chromeWebSecurity: false,
 
